1a79d4069f87dd7f4b76abe0a652f93401d87cec
yusei <realthingshakes25@gmail.com>
2024-05-02 00:08:11 +0900

    (debug) エラーが起こることを確認

--- locustfile.py
+++ locustfile.py
@@ -5,8 +5,8 @@ from locust import HttpUser, events, task
 
 @events.quitting.add_listener
 def _(environment, **kw):
-    if environment.stats.total.fail_ratio > 0.1:
-        logging.error("Test failed due to failure ratio > 10%")
+    if environment.stats.total.fail_ratio > 0.01:
+        logging.error("Test failed due to failure ratio > 1%")
         environment.process_exit_code = 1
     elif environment.stats.total.avg_response_time > 200:
         logging.error("Test failed due to average response time ratio > 200 ms")
